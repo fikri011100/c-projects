590773d3e27735d43b937063cf77284941c6a2fe
Muhammad Fikri Imaduddin Masyhuri <fikri.im@gmail.com>
2020-03-24 14:43:37 +0700

    add project prak3

--- prak3queue/prak3queue.cpp
+++ prak3queue/prak3queue.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <malloc.h>
 #include <string.h>
+
 struct node {
-	int price;
+	int tableNo;
 	char nameUser[12];
-	char nameDress[12];
+	char foodName[12];
+	int qty;
 	struct node *next;
 };
 struct node *start =NULL;
@@ -19,16 +20,15 @@ void initData();
 int main(){
 	initData();
 	int option;
-	system("cls");
-	char * menu[]= {"1. View Queue","2. Add Customer to Queue","3. Serve Cutomer ","4.Exit"};
+	char * menu[]= {"1. Order List","2. Add Order","3. Take Order ","4. Exit"};
 	do{
-		printf("\n\n *****BLUE DRESS SHOP CASHIER QUEUE ***** \n");
-		printf("############################################# \n");
+		printf("\n\nORDER CHINESE FOOD \n");
+		printf("######################################## \n");
 		
 		for(int i= 0; i<=3;i++){
 			printf("%s \n", menu[i]);
 		}
-		printf("\n\n Enter your option : ");
+		printf("\n\n Input your choice : ");
 		scanf("%d", &option);
 		switch(option)
 		{
@@ -40,21 +40,20 @@ int main(){
 			case 3: start = delete_beg(start);
 			break;
 		}
-	}while(option !=5);
+	}while(option !=4);
 	getchar();
 	return 0;
 }
 
 struct node *display(struct node *start){
-	system("cls");
 	struct node *ptr;
 	ptr =start;
 	printf("======================\n\n");
- 	 printf("NO | Customer Name | Dress Name | Price\n");
+ 	 printf("NO | Table Number | Customer Name | FoodName | Quantity\n");
  	 printf("--------------------------------------------------------------\n");
  	 	int i=1;
 	while (ptr != NULL){
-		printf("%d. | %12s | %-12s | Rp. %d \n", i++, ptr->nameUser,ptr->nameDress,ptr->price);
+		printf("%d. | %12d | %-13s | %-19s | %-8d \n", i++, ptr->tableNo,ptr->nameUser,ptr->foodName, ptr->qty);
 		ptr =ptr->next;
 	}
 	return start;
@@ -64,55 +63,64 @@ struct node * delete_beg(struct node *start){
 
 	ptr= start;
 	if(ptr ==NULL ){
-		printf("Ksong");
+		printf("There Are No Order Available.");
 		return start;
 	}
 	start = start -> next;
 	free(ptr);
+	printf("Order Has Been Delivered to Table.");
 	return start;
 }
 struct node * insert_beg(struct node *start){
-	system("cls");
 	struct node *ptr, *new_node;
-	char nameUsr[12],nameDrs[12];
-	int priceD,a,b,c;
+	char nameUsr[12],nameFood[12];
+	int tableNoD,a,b,c, qtyD;
+	do{
+		printf("\n Inpur Table Number[1..20] ");
+		scanf("%d",&tableNoD);
+	}while(tableNoD>=19 && tableNoD<=0 );
 	do{
 		printf("\n Inpur Customer Name ");
 		scanf("%s",nameUsr);	
 		a = strlen(nameUsr);
-	}while(a>=20 || a<=3 );
+	}while(a>=19 || a<=2 );
 	do{
-		printf("\n Inpur Dress Name ");
-		scanf("%s",nameDrs);
-		b = strlen(nameDrs);
-	}while(b>=20 || b<=3 );
+		printf("\n Inpur Food Name[5..20] ");
+		scanf("%s",nameFood);
+		b = strlen(nameFood);
+	}while(b>=19 || b<=3 );
 	do{
-		printf("\n Inpur Dress Price ");
-		scanf("%d",&priceD);
-	}while(priceD>=999 && priceD<=50 );
+		printf("\n Inpur Quantity[1..1000] ");
+		scanf("%d",&qtyD);
+	}while(qtyD>=1000 && qtyD<=0 );
+	
 	
 	new_node =(struct node *)malloc(sizeof(struct node));
 	strcpy(new_node ->nameUser,nameUsr);
-	strcpy(new_node ->nameDress,nameDrs);
-	new_node -> price = priceD;
+	strcpy(new_node ->foodName,nameFood);
+	new_node -> tableNo = tableNoD;
+	new_node -> qty = qtyD;
 	new_node -> next = NULL;
     ptr = start;
     while(ptr -> next != NULL)
     ptr = ptr -> next;
     ptr -> next = new_node;
+	printf("\n\n--- Add New Order Success ---\n");
 	return start;
 }
 void initData(){
 	struct node * new_node;	
 		
-	int	price1[3]={3,15,16};
-	char * name[]={"martin","suci","diah"};
-	char * nameDre[]={"Batik","Kain","Kaos"};
+	int	tableNo1[3]={7,2,9};
+	int qty1[3]={1,2,9};
+	char * name[]={"Linda","Nad","Jarjit"};
+	char * nameFoodd[]={"AyamGoreng","GuleIkan","Sambalado"};
 	for (int i =0;i<3;i++){
 		new_node = (struct node *)malloc(sizeof(struct node));
 		strcpy(new_node ->nameUser,name[i]);
-		strcpy(new_node ->nameDress,nameDre[i]);
-		new_node ->price=price1[i];
+		strcpy(new_node ->foodName,nameFoodd[i]);
+		new_node ->tableNo=tableNo1[i];
+		new_node ->qty=qty1[i];
 		new_node ->next= start;
 		start= new_node;
 	}	
